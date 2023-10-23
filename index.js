cafb4c896b0cfc1af6d40312413ee01ea1ad0726
King Gambit Lawrence <kinggambitlawrence@kings-air.mynetworksettings.com>
2023-10-23 16:07:21 -0400

    intial

---
+++ index.js
@@ -0,0 +1,15 @@
+//create a string telling someone they are the chosen one to open the vault.
+//assign 3 varibles
+//associate each variable to a math equation to open up the vault
+
+const message = "Good Afternoon, YOU HAVE BEEN CHOOSEN, OUT OF MANY TO BE THE ONE WE HAVE HIRED TO OPEN THE VAULT. ITS BEEN LOCKED FOR DECADES AND ONLY YOU CAN FIGURE OUT THE COMBINATION.";
+console.log(message)
+const codeOne = 5 * 2;
+const codeTwo = 100 % 2 + 40;
+const codeThree = 80 / 2 - 1;
+
+console.log (codeOne, codeTwo, codeThree)
+
+const finalMessage = `${codeOne} ${codeTwo} ${codeThree} CONGRATES YOU'RE IN!!`
+
+alert(finalMessage)
